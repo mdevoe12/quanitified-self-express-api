0d54cf12208b451dd259703f4621f1ec4fb46a9d
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-17 11:14:52 -0600

    initial server.js setup

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
+app.set('port', process.env.PORT || 3000)
+app.locals.title = "Quantified Self Express API"
+
 app.get('/api/v1/foods', function(request, response) {
   response.send('Endpoint for api/v1/foods')
 })
 
-app.listen(3000)
+
+app.listen(3000, function() {
+  console.log(`${app.locals.title} is running on port 3000.`)
+})
