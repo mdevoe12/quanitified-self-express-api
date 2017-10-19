5b4bd5f5cb73caf15fcdfa3fcc019a545b450464
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-19 14:07:43 -0600

    seeds file populating foods, meals and meal_foods complete

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -1,5 +1,8 @@
+let randArray = [1, 2, 3]
+let randNum;
+
 exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE foods RESTART IDENTITY')
+  return knex.raw('TRUNCATE meal_foods RESTART IDENTITY CASCADE; TRUNCATE foods RESTART IDENTITY CASCADE; TRUNCATE meals RESTART IDENTITY CASCADE')
   .then(function() {
     return Promise.all([
       knex.raw(
@@ -13,6 +16,50 @@ exports.seed = function(knex, Promise) {
       knex.raw(
         'INSERT INTO foods (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
         ["Brown Rice", 150, new Date, new Date]
+      ),
+      knex.raw(
+        'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
+        ["Breakfast", new Date, new Date]
+      ),
+      knex.raw(
+        'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
+        ["Lunch", new Date, new Date]
+      ),
+      knex.raw(
+        'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
+        ["Dinner", new Date, new Date]
+      ),
+      knex.raw(
+        'INSERT INTO meals (name, created_at, updated_at) VALUES (?, ?, ?)',
+        ["Snacks", new Date, new Date]
+      ),
+      randNum = function() {return randArray[Math.floor(Math.random() * randArray.length)]},
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
+      ),
+      knex.raw('INSERT INTO meal_foods (food_id, meal_id) VALUES (?, ?)',
+        [randNum(), randNum()]
       )
     ])
   })
